fb5755c647dd7dd3e517010b2179021899f600a4
Andrew <andrewvieira1@hotmail.com>
2024-11-05 20:59:01 -0300

    Andrew
    .exe

---
+++ Semana 01/Teste/build/CMakeFiles/OlaMundo.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OlaMundo.dir/main.cpp.obj"
+  "CMakeFiles/OlaMundo.dir/main.cpp.obj.d"
+  "OlaMundo.exe"
+  "OlaMundo.exe.manifest"
+  "OlaMundo.pdb"
+  "libOlaMundo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OlaMundo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Semana 01/Teste/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "OLA, Mundo!" << std::endl;
+    return 0;
+}
